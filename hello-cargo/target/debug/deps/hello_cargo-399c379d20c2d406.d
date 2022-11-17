820ec18003d22c231fe52327230781b9989fb25e
kulik-lab <w.kulik@posteo.net>
2022-11-17 22:46:43 +0000

    initial

---
+++ hello-cargo/target/debug/deps/hello_cargo-399c379d20c2d406.d
@@ -0,0 +1,5 @@
+/home/k1/rust-proj/hello-cargo/target/debug/deps/hello_cargo-399c379d20c2d406.rmeta: src/main.rs
+
+/home/k1/rust-proj/hello-cargo/target/debug/deps/hello_cargo-399c379d20c2d406.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/deps/hello_cargo-684e2891257f87c3.d
@@ -0,0 +1,5 @@
+/home/k1/rust-proj/hello-cargo/target/debug/deps/hello_cargo-684e2891257f87c3.rmeta: src/main.rs
+
+/home/k1/rust-proj/hello-cargo/target/debug/deps/hello_cargo-684e2891257f87c3.d: src/main.rs
+
+src/main.rs:
