e01cc80a50030e604bcb6e6795e52502f4fd6876
kulik-lab <w.kulik@posteo.net>
2022-11-27 21:44:24 +0000

    create datatypes section

---
+++ rust-tutorial/target/debug/deps/rust_tutorial-cd9156acf2505c0e.d
@@ -0,0 +1,5 @@
+/home/k1/rust-proj/rust-tutorial/target/debug/deps/rust_tutorial-cd9156acf2505c0e.rmeta: src/main.rs
+
+/home/k1/rust-proj/rust-tutorial/target/debug/deps/rust_tutorial-cd9156acf2505c0e.d: src/main.rs
+
+src/main.rs:
---
+++ rust-tutorial/target/debug/deps/rust_tutorial-e64792b35ac44def.d
@@ -0,0 +1,5 @@
+/home/k1/rust-proj/rust-tutorial/target/debug/deps/rust_tutorial-e64792b35ac44def.rmeta: src/main.rs
+
+/home/k1/rust-proj/rust-tutorial/target/debug/deps/rust_tutorial-e64792b35ac44def.d: src/main.rs
+
+src/main.rs:
